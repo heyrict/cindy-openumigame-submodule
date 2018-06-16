fecccdc6d0ce659d3db20bd9711d1ef335d0d1af
heyrict <xiezh0831@126.com>
2018-06-16 17:31:43 +0800

    Add API for puzzle_show
    - puzzle_show_push_ques
    - puzzle_show_push_answ
    - puzzle_show_push_chatmessage

--- urls.py
+++ urls.py
@@ -4,7 +4,10 @@ from . import views
 
 app_name = "open"
 
-contentpatterns = [
+urlpatterns = [
     path("mondai", views.mondai, name="mondai"),
     path("mondai/show/<int:id>", views.mondai_show, name="mondai_show"),
+    path("mondai/show/<int:id>/push_ques", views.mondai_show_push_ques, name="mondai_show_push_ques"),
+    path("mondai/show/<int:id>/push_answ", views.mondai_show_push_answ, name="mondai_show_push_answ"),
+    path("mondai/show/<int:id>/push_chatmessage", views.mondai_show_push_chatmessage, name="mondai_show_push_chatmessage"),
 ] # yapf: disable
--- views.py
+++ views.py
@@ -1,10 +1,12 @@
+import re
 from itertools import chain
 
 from django.core.paginator import Paginator
 from django.db.models import Case, Count, When
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
 
-from sui_hei.models import ChatRoom, Dialogue, Hint, Puzzle
+from sui_hei.models import ChatMessage, ChatRoom, Dialogue, Hint, Puzzle
 
 # Create your views here.
 
@@ -48,3 +50,85 @@ def mondai_show(request, *args, **kwargs):
             "union_list": sorted(chain(dialogue_list, hint_list), key=lambda x: x.created),
             "chatroom": chatroom,
         }) # yapf: disable
+
+
+def mondai_show_push_answ(request, **kwargs):
+    if request.method == "POST" and request.user.is_authenticated:
+        puzzleId = kwargs.get('id')
+        if not puzzleId:
+            return redirect('/')
+        try:
+            to_update = {}
+            PALEN = len('push_answ_')
+            CGLEN = len('check_goodques_')
+            CTLEN = len('check_trueansw_')
+            for pk in request.POST.keys():
+                if pk[:PALEN] == 'push_answ_':
+                    pk = pk[PALEN:]
+                    if pk not in to_update:
+                        to_update[pk] = get_object_or_404(Dialogue, id=pk)
+                    answer = request.POST.get('push_answ_' + pk)
+                    if answer: to_update[pk].answer = answer
+                elif pk[:CGLEN] == 'check_goodques_':
+                    pk = pk[CGLEN:]
+                    if pk not in to_update:
+                        to_update[pk] = get_object_or_404(Dialogue, id=pk)
+                    to_update[pk].good = not to_update[pk].good
+                elif pk[:CTLEN] == 'check_trueansw_':
+                    pk = pk[CTLEN:]
+                    if pk not in to_update:
+                        to_update[pk] = get_object_or_404(Dialogue, id=pk)
+                    to_update[pk].true = not to_update[pk].true
+
+            for _, obj in to_update.items():
+                if not obj.answer:
+                    obj.answeredtime = timezone.now()
+                else:
+                    obj.answerEditTimes += 1
+                obj.save()
+        except Exception as e:
+            print("PushAnsw:", e)
+    return redirect(request.META['HTTP_REFERER'].split('?', 1)[0])
+
+
+def mondai_show_push_ques(request, **kwargs):
+    if request.method == "POST" and request.user.is_authenticated:
+        puzzleId = kwargs.get('id')
+        if not puzzleId:
+            return redirect('/')
+        try:
+            puzzle = get_object_or_404(Puzzle, id=puzzleId)
+            content = request.POST['push_ques']
+            if content == '': raise ValueError("Empty Input Data")
+
+            ques = Dialogue(
+                user=request.user,
+                question=content.strip(),
+                created=timezone.now(),
+                puzzle=puzzle)
+            ques.save()
+        except Exception as e:
+            print("PushQues:", e)
+    return redirect(request.META['HTTP_REFERER'].split('?', 1)[0])
+
+
+def mondai_show_push_chatmessage(request, **kwargs):
+    if request.method == "POST" and request.user.is_authenticated:
+        puzzleId = kwargs.get('id')
+        chatroom = ChatRoom.objects.get(name="puzzle-%d" % puzzleId)
+        if not puzzleId:
+            return redirect('/')
+        try:
+            puzzle = get_object_or_404(Puzzle, id=puzzleId)
+            content = request.POST['push_chatmessage']
+            if content == '': raise ValueError("Empty Input Data")
+
+            chatmessage = ChatMessage(
+                user=request.user,
+                content=content.strip(),
+                created=timezone.now(),
+                chatroom=chatroom)
+            chatmessage.save()
+        except Exception as e:
+            print("PushQues:", e)
+    return redirect(request.META['HTTP_REFERER'].split('?', 1)[0])
